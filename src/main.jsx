90008bf188684ac5f50e6768d15225409b3afbc1
kamlesh kumar <147686221+Devloperkamlesh@users.noreply.github.com>
2025-05-17 21:25:19 +0530

    ss

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
-import { router } from "./components/Router";
+import { router } from "./components/router";
 
 const rootElement = document.getElementById("root");
 
